8abc011116bc6ced4967d8c41420f3937fe5e655
Edson Bastos <edsonbastos2@hotmail.com>
2020-05-16 18:20:25 -0300

    Erro!

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import controlModal from './modules/modal.js';
 import initDropDown from './modules/dropdown.js';
+import initTooltip from './modules/tooltip.js';
 
 
 initScrollSuave();
@@ -11,4 +12,5 @@ animacaoScroll();
 initAccordion();
 initTabNav();
 controlModal();
-initDropDown()
+initDropDown();
+initTooltip()
