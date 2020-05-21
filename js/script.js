d254c7a537b411b2009b55abfc9c0af53e09f6f6
Edson Bastos <edsonbastos2@hotmail.com>
2020-05-20 21:50:14 -0300

    Adicionado a classe active com transform rotage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from './modules/tabnav.js';
 import controlModal from './modules/modal.js';
 import initDropDown from './modules/dropdown.js';
 import initTooltip from './modules/tooltip.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 
 initScrollSuave();
@@ -14,3 +15,4 @@ initTabNav();
 controlModal();
 initDropDown();
 initTooltip();
+initMenuMobile();
